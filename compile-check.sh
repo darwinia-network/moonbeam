aaa5bfa82da81503996829efd43b334fbacffde9
bear <boundless.forest@outlook.com>
2022-11-21 13:20:51 +0800

    Archor `polkadot-v0.30.0`

---
+++ compile-check.sh
@@ -0,0 +1,26 @@
+set -e
+
+echo ">>> Start compile process"
+
+crates=(
+    # moonbeam-evm-tracer
+    # evm-tracing-events
+    # moonbeam-primitives-ext
+    # moonbeam-rpc-core-debug
+    # moonbeam-rpc-core-trace
+    # moonbeam-rpc-core-types
+    # moonbeam-rpc-primitives-debug
+    # moonbeam-client-evm-tracing
+    # moonbeam-rpc-debug
+    # moonbeam-rpc-trace
+    precompile-utils-macro
+    precompile-utils
+)
+
+for c in ${crates[@]}
+do
+    echo ">>>>>>>> Start $c"
+    cargo check -p $c
+    echo ">>>>>>>> End $c"
+
+done 
