83f9761a1bc88031ba03dfac93c4138c00476d23
bear <boundless.forest@outlook.com>
2023-11-07 15:47:07 +0800

    Try fix compile

--- precompiles/conviction-voting/src/lib.rs
+++ precompiles/conviction-voting/src/lib.rs
@@ -134,7 +134,7 @@ where
 		let origin = Runtime::AddressMapping::into_account_id(caller);
 		let call = ConvictionVotingCall::<Runtime>::vote { poll_index, vote }.into();
 
-		<RuntimeHelper<Runtime>>::try_dispatch(handle, Some(origin).into(), call, 0)?;
+		<RuntimeHelper<Runtime>>::try_dispatch(handle, Some(origin).into(), call)?;
 
 		event.record(handle)?;
 
@@ -288,7 +288,7 @@ where
 		let origin = Runtime::AddressMapping::into_account_id(caller);
 		let call = ConvictionVotingCall::<Runtime>::remove_vote { class, index };
 
-		RuntimeHelper::<Runtime>::try_dispatch(handle, Some(origin).into(), call, 0)?;
+		RuntimeHelper::<Runtime>::try_dispatch(handle, Some(origin).into(), call)?;
 
 		event.record(handle)?;
 
@@ -332,7 +332,7 @@ where
 			index,
 		};
 
-		RuntimeHelper::<Runtime>::try_dispatch(handle, Some(origin).into(), call, 0)?;
+		RuntimeHelper::<Runtime>::try_dispatch(handle, Some(origin).into(), call)?;
 
 		event.record(handle)?;
 
@@ -380,8 +380,7 @@ where
 		RuntimeHelper::<Runtime>::try_dispatch(
 			handle,
 			Some(origin).into(),
-			call,
-			SYSTEM_ACCOUNT_SIZE,
+			call
 		)?;
 
 		event.record(handle)?;
@@ -405,7 +404,7 @@ where
 		let origin = Runtime::AddressMapping::into_account_id(caller);
 		let call = ConvictionVotingCall::<Runtime>::undelegate { class };
 
-		RuntimeHelper::<Runtime>::try_dispatch(handle, Some(origin).into(), call, 0)?;
+		RuntimeHelper::<Runtime>::try_dispatch(handle, Some(origin).into(), call)?;
 
 		event.record(handle)?;
 
@@ -437,7 +436,7 @@ where
 		let origin = Runtime::AddressMapping::into_account_id(handle.context().caller);
 		let call = ConvictionVotingCall::<Runtime>::unlock { class, target };
 
-		RuntimeHelper::<Runtime>::try_dispatch(handle, Some(origin).into(), call, 0)?;
+		RuntimeHelper::<Runtime>::try_dispatch(handle, Some(origin).into(), call)?;
 
 		event.record(handle)?;
 
